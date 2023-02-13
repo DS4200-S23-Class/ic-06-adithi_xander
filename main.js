7604dacc6415bb052664586bfa29e8cc794b01c9
XanderSimon <rednax01@gmail.com>
2023-02-13 15:20:21 -0500

    tried our best, didn't work

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ function buttonClick() {
   const frame = document.getElementById('frame');
   frame.style.borderColor = "red";
 }
+
+function mouveOver() {
+  const circle = document.getElementById('circ')
+  circle.style.fill = "Orange";
+}
+
+function mouveOver() {
+  const circle = document.getElementById('circ')
+  circle.style.fill = "Yellow";
+}
+
+addEventListener('mouseover', mouseOver())
+addEventListener('mouseout', mouseOut())
